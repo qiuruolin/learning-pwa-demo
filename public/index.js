a0957bc846e421e5563832a1447b581dca12cfd6
qiuruolin <qiu.ruolin@sumixer.com>
2018-10-24 16:37:47 +0800

    添加离线缓存功能

--- public/index.js
+++ public/index.js
@@ -76,7 +76,6 @@
     function queryBook() {
         var input = document.querySelector('#js-search-input');
         var query = input.value;
-        var xhr = new XMLHttpRequest();
         var url = '/book?q=' + query + '&fields=id,title,image,author,publisher,price';
         if (query === '') {
             tip('请输入关键词');
@@ -85,30 +84,33 @@
         document.querySelector('#js-list').innerHTML = '';
         document.querySelector('#js-thanks').style = 'display: none';
         loading(true);
-        xhr.timeout = 60000;
-        xhr.onreadystatechange = function () {
-            var response = {};
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                loading(false);
-                try {
-                    response = JSON.parse(xhr.responseText);
-                }
-                catch (e) {
-                    response = xhr.responseText;
-                }
-                tip();
-                if (response.books.length === 0) {
-                    tip('无结果');
-                }
-                else {
+
+        let cacheData
+        var remotePromise = getApiDataRemote(url)
+        getApiDataFromCache(url).then(function(data){
+            if(data){
+                loading(false)
+                input.blur();
+                fillList(data.books);
+                document.querySelector('#js-thanks').style = 'display: block';
+            }
+            cacheData = data || {}
+            return remotePromise
+        }).then(function(data){
+            if(data){
+                if(JSON.stringify(data) !== JSON.stringify(cacheData)){
+                    loading(false)
                     input.blur();
-                    fillList(response.books);
+                    fillList(data.books);
                     document.querySelector('#js-thanks').style = 'display: block';
                 }
             }
-        };
-        xhr.open('GET', url, true);
-        xhr.send(null);
+            else if(!cacheData.books){
+                //当无网络且无cache时
+                loading(false)
+                tip('无结果')
+            }
+        })
     }
 
     /**
@@ -126,4 +128,53 @@
             queryBook();
         }
     });
+
+    // 注册serviceWorker
+    if('serviceWorker' in navigator){
+        navigator.serviceWorker.register('./sw.js').then(function(){
+            console.log('[ServiceWorker] 注册成功')
+        })
+    }
+
+    //读取缓存数据
+    function getApiDataFromCache(url){
+        if('caches' in window){
+            return caches.match(url).then(function(cache){
+                if(!cache){
+                    return
+                }
+                return cache.json()
+            })
+        }
+        else{
+            return Promise.resolve()
+        }
+    }
+    // 获取远程数据
+    function getApiDataRemote(url){
+        return new Promise(function(resolve, reject){
+            var xhr = new XMLHttpRequest();
+        xhr.timeout = 60000;
+        xhr.onreadystatechange = function () {
+            var response = {};
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                try {
+                    response = JSON.parse(xhr.responseText);
+                }
+                catch (e) {
+                    response = xhr.responseText;
+                }
+                resolve(response)
+            }
+            else if(xhr.readyState === 4){
+                resolve()
+            }
+        };
+        xhr.onabort = reject
+        xhr.onerror = reject
+        xhr.ontimeout = reject
+        xhr.open('GET', url, true);
+        xhr.send(null);
+        })
+    }
 })();
---
+++ public/sw.js
@@ -0,0 +1,67 @@
+// ServiceWorker生命周期：installing->installed->activing->activated->redundant
+// 当Service Worker安装（installed）完毕后，会触发install事件；而激活（activated）后，则会触发activate事件
+// self是Service Worker中一个特殊的全局变量
+
+const cacheName = 'PWA_CACHE_NAME'
+const apiCacheName = 'API_CACHE_NAME'
+// 缓存列表
+let cacheFiles = [
+    '/',
+    './index.html',
+    './index.js',
+    './style.css',
+    './img/book.png',
+    './img/loading.svg'
+]
+
+self.addEventListener('install', function(e){
+    console.log('[ServiceWorker] 状态： install')
+    // 通过caches.open()与cache.addAll()方法将资源缓存起来
+    e.waitUntil(caches.open(cacheName).then(function(cache){
+        return cache.addAll(cacheFiles)
+    }))
+})
+
+// 检查cacheName是否变化，更新缓存
+self.addEventListener('activate', function(e){
+    console.log('[ServiceWorker] 状态：activate')
+    //清理旧版本
+    e.waitUntil(caches.keys().then(function(keys){
+        return Promise.all(keys.map(function(key){
+            if(key !== cacheName){
+                return caches.delete(key)
+            }
+        }))
+    }))
+    return self.clients.claim() //更新客户端
+})
+
+self.addEventListener('fetch', function(e){
+    //需要缓存的xhr请求
+    const cacheRequestUrls = [
+        '/book?'
+    ]
+    console.log('现在正在请求API：' + e.request.url)
+    //判断当前请求是否需要缓存
+    let needCache = cacheRequestUrls.some(function(url){
+        return e.request.url.indexOf(url) > -1
+    })
+
+    if(needCache){
+        caches.open(apiCacheName).then(function(cache){
+            return fetch(e.request).then(function(res){
+                cache.put(e.request.url, res.clone())
+                return res
+            })
+        })
+    }
+    else{
+      // respondWith方法向浏览器返回数据, caches.match(e.request)则可以查看当前的请求是否有一份本地缓存
+      e.respondWith(caches.match(e.request).then(function(cache){
+          return cache || fetch(e.request)
+      }).catch(function(err){
+          console.log(err)
+          return fetch(e.request)
+      }))
+    }
+})
